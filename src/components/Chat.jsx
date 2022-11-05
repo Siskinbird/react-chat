9a39ed1386aec70c59af7f30f4641e6154dc17e3
Dmitrii Chizhov <dimachig@gmail.com>
2022-11-05 18:08:57 +0300

    add: Input component styles

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,6 +3,7 @@ import Cam from '../images/cam.png'
 import Add from '../images/add.png'
 import More from '../images/more.png'
 import Messages from "./Messages";
+import Input from "./Input";
 
 function Chat(props) {
     return (
@@ -16,6 +17,7 @@ function Chat(props) {
                 </div>
             </div>
             <Messages/>
+            <Input/>
         </div>
     );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Img from '../images/img.png'
+import Attach from '../images/attach.png'
+
 
 function Input(props) {
     return (
-        <div>Input</div>
+        <div className='input'>
+            <input type='text' placeholder='Type something'/>
+            <div className="send">
+                <img src={Attach} alt='Image'/>
+                <input type='file' style={{display: 'none'}} id='file' />
+                <label htmlFor='file'>
+                    <img src={Img} alt='image'/>
+                </label>
+                <button>Send</button>
+            </div>
+        </div>
     );
 }
 
