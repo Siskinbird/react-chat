667a4d11ed043abd1dd66b7ec458f67ef62d6f9a
Dmitrii Chizhov <dimachig@gmail.com>
2022-11-06 16:56:51 +0300

    add: Login user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import {signOut} from 'firebase/auth'
+import {auth} from "../firebase";
 
-function Navbar(props) {
+function Navbar() {
     return (
         <div className='navbar'>
             <span className="logo">Siskin chat</span>
             <div className="user">
                 <img src='https://anime-star.ru/wp-content/uploads/2021/12/YArkie-fioletovye-avatarki-anime_08.jpg' alt='avatar'/>
                 <span>User name</span>
-                <button>logout</button>
+                <button onClick={() => signOut(auth)}>logout</button>
             </div>
         </div>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {auth, db, storage} from "../firebase";
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
+import {doc, setDoc} from "firebase/firestore";
 
 
 function Login(props) {
+    const [err, setErr] = useState(false)
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const displayEmail = e.target[0].value
+        const displayPassword = e.target[1].value
+
+        // console.log(e.target[0].value)
+
+        try {
+            await signInWithEmailAndPassword(auth, displayEmail, displayPassword)
+            navigate('/')
+        } catch (err) {
+            setErr(true)
+        }
+    }
     return (
         <div className='formContainer'>
             <div className="formWrapper">
                 <span className='logo'>Siskin Chat</span>
                 <span className='title'>Login</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="email" placeholder='email'/>
-                    <input type="password" placeholder='password'/>
+                    <input type="password" placeholder='password' autoComplete='on'/>
                     <button>Sign in</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
-                <p>You don't have an account? Register</p>
+                <p>You don't have an account? <Link to='/register'>Register</Link></p>
             </div>
         </div>
     );
