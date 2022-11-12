81db1297a0b2847c8c90fe2d88a384e531b4e422
Dmitrii Chizhov <dimachig@gmail.com>
2022-11-12 18:22:11 +0300

    add: Add chats for users

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AuthContext} from "../context/AuthContext";
+import {ChatContext} from "../context/ChatContext";
 
-function Message(props) {
+const Message = ({message}) => {
+
+    const {currentUser} = useContext(AuthContext)
+    const {data} = useContext(ChatContext)
     return (
         <div className='message owner'>
-            <div className="messageInfo">
-                <img src='https://papik.pro/uploads/posts/2021-09/1630756344_14-papik-pro-p-anime-risunki-kvadratnie-26.jpg' alt='Image'/>
-                <span>Date and time</span>
-            </div>
-            <div className="messageContent">
-                <p>hello</p>
-                {/*<img src='https://papik.pro/uploads/posts/2021-09/1630756344_14-papik-pro-p-anime-risunki-kvadratnie-26.jpg' alt='Image'/>*/}
-            </div>
+            {/*<div className="messageInfo">*/}
+            {/*    <img src='https://papik.pro/uploads/posts/2021-09/1630756344_14-papik-pro-p-anime-risunki-kvadratnie-26.jpg' alt='Image'/>*/}
+            {/*    <span>Date and time</span>*/}
+            {/*</div>*/}
+            {/*<div className="messageContent">*/}
+            {/*    <p>hello</p>*/}
+            {/*    /!*<img src='https://papik.pro/uploads/posts/2021-09/1630756344_14-papik-pro-p-anime-risunki-kvadratnie-26.jpg' alt='Image'/>*!/*/}
+            {/*</div>*/}
         </div>
     );
 }
