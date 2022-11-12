ec7cc296ad3a26af659b22862815d9bb8a87411e
Dmitrii Chizhov <dimachig@gmail.com>
2022-11-12 18:51:01 +0300

    fix: all warning

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import {AuthContext} from "../context/AuthContext";
 import {ChatContext} from "../context/ChatContext";
 
@@ -6,16 +6,23 @@ const Message = ({message}) => {
 
     const {currentUser} = useContext(AuthContext)
     const {data} = useContext(ChatContext)
+
+    const ref = useRef()
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({behavior: "smooth"})
+    }, [message])
+
     return (
-        <div className='message owner'>
-            {/*<div className="messageInfo">*/}
-            {/*    <img src='https://papik.pro/uploads/posts/2021-09/1630756344_14-papik-pro-p-anime-risunki-kvadratnie-26.jpg' alt='Image'/>*/}
-            {/*    <span>Date and time</span>*/}
-            {/*</div>*/}
-            {/*<div className="messageContent">*/}
-            {/*    <p>hello</p>*/}
-            {/*    /!*<img src='https://papik.pro/uploads/posts/2021-09/1630756344_14-papik-pro-p-anime-risunki-kvadratnie-26.jpg' alt='Image'/>*!/*/}
-            {/*</div>*/}
+        <div ref={ref} className={`message ${message.senderId === currentUser.uid && 'owner'}`}>
+            <div className="messageInfo">
+                <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt='user'/>
+                <span>Date and time</span>
+            </div>
+            <div className="messageContent">
+                <p>{message.text}</p>
+                {message.image && <img src={message.image} alt='Sending'/>}
+            </div>
         </div>
     );
 }
